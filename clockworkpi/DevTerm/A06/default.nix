c422145fdbf8074ee54b7ab430c1c067f5ff6d11
Tom Charnock <tom@charnock.fr>
2023-08-14 20:41:54 +0200

    Updating SHA

--- clockworkpi/DevTerm/A06/default.nix
+++ clockworkpi/DevTerm/A06/default.nix
@@ -47,7 +47,7 @@
         name = "a06-power-charger";
         patch = (pkgs.fetchpatch {
           url = "https://gitlab.manjaro.org/manjaro-arm/packages/core/linux-clockworkpi-a06/-/raw/master/0004-power-supply-Add-Support-for-RK817-Charger.patch";
-          sha256 = lib.fakeSha256;
+          sha256 = "KjhW51LY+WK0XCbcyqm56OV2rR0wiiJCv7rYN/IoOzI=";
         });
       }
     ];
