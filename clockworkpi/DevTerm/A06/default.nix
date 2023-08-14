0861ab9d64bab08e01b4f7e18f72ee5ffc936966
Tom Charnock <tom@charnock.fr>
2023-08-14 20:47:06 +0200

    Removing surplus patches

--- clockworkpi/DevTerm/A06/default.nix
+++ clockworkpi/DevTerm/A06/default.nix
@@ -43,13 +43,13 @@
           BACKLIGHT_OCP8178 = lib.kernel.module;
         };
       }
-      {
-        name = "a06-power-charger";
-        patch = (pkgs.fetchpatch {
-          url = "https://gitlab.manjaro.org/manjaro-arm/packages/core/linux-clockworkpi-a06/-/raw/master/0004-power-supply-Add-Support-for-RK817-Charger.patch";
-          sha256 = "KjhW51LY+WK0XCbcyqm56OV2rR0wiiJCv7rYN/IoOzI=";
-        });
-      }
+      #{
+      #  name = "a06-power-charger";
+      #  patch = (pkgs.fetchpatch {
+      #    url = "https://gitlab.manjaro.org/manjaro-arm/packages/core/linux-clockworkpi-a06/-/raw/master/0004-power-supply-Add-Support-for-RK817-Charger.patch";
+      #    sha256 = "KjhW51LY+WK0XCbcyqm56OV2rR0wiiJCv7rYN/IoOzI=";
+      #  });
+      #}
     ];
   };
   hardware.enableRedistributableFirmware = true;
