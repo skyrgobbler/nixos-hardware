e83b97b1cffa75b653f81d58fafa56f70eb100e8
Tom Charnock <tom@charnock.fr>
2023-08-14 18:06:00 +0200

    Changed sha256 for fake

--- clockworkpi/DevTerm/A06/default.nix
+++ clockworkpi/DevTerm/A06/default.nix
@@ -8,14 +8,14 @@
         name = "a06-dts";
         patch = (pkgs.fetchpatch {
           url = "https://raw.githubusercontent.com/clockworkpi/DevTerm/main/Code/patch/armbian_build_a06/patch/kernel-001-a06-dts.patch";
-          sha256 = "10gzx98apb47795w69cpafscp285i203r8b6pzan5pwk324z9b19";
+          sha256 = lib.fakeSha256;
         });
       }
       {
         name = "a06-power";
         patch = (pkgs.fetchpatch {
           url = "https://raw.githubusercontent.com/clockworkpi/DevTerm/main/Code/patch/armbian_build_a06/patch/kernel-002-power.patch";
-          sha256 = "1il25l90s0cn7c61rzj2fhs51147vydc8zmiyw4xxc59rfpm96a7";
+          sha256 = lib.fakeSha256;
         });
         extraStructuredConfig = {
           MFD_AXP20X = lib.kernel.yes;
@@ -27,7 +27,7 @@
         name = "a06-audio";
         patch = (pkgs.fetchpatch {
           url = "https://raw.githubusercontent.com/clockworkpi/DevTerm/main/Code/patch/armbian_build_a06/patch/kernel-003-audio.patch";
-          sha256 = "1fbdfmfx5lkh3q4zf8ify6s4ngi6bf0kcjyl9wpvgd63dsyfw6lv";
+          sha256 = lib.fakeSha256;
         });
         extraStructuredConfig = {
           SND_SOC_ES8323 = lib.kernel.module;
@@ -37,7 +37,7 @@
         name = "a06-panel";
         patch = (pkgs.fetchpatch {
           url = "https://raw.githubusercontent.com/clockworkpi/DevTerm/main/Code/patch/armbian_build_a06/patch/kernel-004-panel.patch";
-          sha256 = "0fdvgr36x68rwqyw1fg60m4rxq5kd1x5slbdrfxpw8s3sdcsr1g6";
+          sha256 = lib.fakeSha256;
         });
         extraStructuredConfig = {
           DRM_PANEL_CWD686 = lib.kernel.module;
@@ -47,7 +47,7 @@
         name = "a06-backlight";
         patch = (pkgs.fetchpatch {
           url = "https://raw.githubusercontent.com/clockworkpi/DevTerm/main/Code/patch/armbian_build_a06/patch/kernel-005-backlight.patch";
-          sha256 = "0hb8whf2gmh2vmn5jajqwxd5lybw0sasp106vw2l9y4936bm5w5v";
+          sha256 = lib.fakeSha256;
         });
         extraStructuredConfig = {
           BACKLIGHT_OCP8178 = lib.kernel.module;
@@ -57,7 +57,7 @@
         name = "a06-panel_2";
         patch = (pkgs.fetchpatch {
           url = "https://raw.githubusercontent.com/clockworkpi/DevTerm/main/Code/patch/armbian_build_a06/patch/kernel-006-panel.patch";
-          sha256 = "0ihiih8j8s55kkn8d4i2f9a295635galr81zgv4zzy6fw520ihcn";
+          sha256 = lib.fakeSha256;
         });
       }
     ];
